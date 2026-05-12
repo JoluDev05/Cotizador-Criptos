bbd2b3aa5f4064351d8e1ae8bd3d3d9c497d993c
Jorge Arce <jorgearcedev1@gmail.com>
2026-05-12 14:58:27 -0700

    schema zod y mostrando cambio de monedas

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,3 +1,4 @@
+import { currencies } from '../data';
 
 export default function CriptoSearchForm() {
   return (
@@ -12,6 +13,10 @@ export default function CriptoSearchForm() {
                     id="currency"
                 >
                     <option value="">-- Seleccione -- </option>
+                    {currencies.map(currency => (
+                        <option key={currency.code} value={currency.code}>{currency.name}</option>
+                    ))
+                    }
                 </select>
         </div>
         <div className="field">
